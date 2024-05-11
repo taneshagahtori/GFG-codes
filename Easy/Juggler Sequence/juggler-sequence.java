839e5617ef3470dea9b975fe9a6a03a41f740f9a
Tanesha Gahtori <104068017+taneshagahtori@users.noreply.github.com>
2024-05-11 19:57:38 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Juggler Sequence/juggler-sequence.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while (t-- > 0) {
+            long n = Long.parseLong(in.readLine());
+
+            Solution ob = new Solution();
+            List<Long> ans = new ArrayList<>();
+            StringBuilder out = new StringBuilder();
+            ans = ob.jugglerSequence(n);
+            for (int i = 0; i < ans.size(); i++) out.append(ans.get(i) + " ");
+            System.out.println(out);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    static List<Long> jugglerSequence(long n) {
+        // code here
+        List<Long> arr= new ArrayList<Long>();
+        arr.add(n);
+        while(n>1){
+            if(n%2==0){
+                n =(long) Math.pow(n,(float)1/2);
+                arr.add(n);
+            }
+            else{
+                n= (long)Math.pow(n,(float)3/2);
+                arr.add(n);
+            }
+        }
+        return arr;
+
+    }
+}
