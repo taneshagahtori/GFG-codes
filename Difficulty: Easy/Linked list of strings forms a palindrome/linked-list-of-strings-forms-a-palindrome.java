56934df69729ebdab4f7c1d9a858bc5e6b62913f
Tanesha Gahtori <104068017+taneshagahtori@users.noreply.github.com>
2024-07-02 16:15:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Linked list of strings forms a palindrome/linked-list-of-strings-forms-a-palindrome.java
@@ -0,0 +1,90 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node {
+    String data;
+    Node next;
+
+    Node(String x) {
+        data = x;
+        next = null;
+    }
+}
+
+class LinkedList {
+    Node head;
+
+    LinkedList() { head = null; }
+
+    void addNode(String str) {
+        if (head == null) {
+            head = new Node(str);
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = new Node(str);
+        }
+    }
+
+    void print() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        while (T-- > 0) {
+            int K = sc.nextInt();
+            LinkedList list = new LinkedList();
+
+            for (int i = 0; i < K; i++) {
+                String str = sc.next();
+                list.addNode(str);
+            }
+
+            Solution ob = new Solution();
+            boolean ans = ob.compute(list.head);
+            if (ans) {
+                System.out.println("true");
+            } else {
+                System.out.println("false");
+            }
+        }
+        sc.close();
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    public boolean compute(Node root) {
+        String str = "";
+        Node temp = root;
+// 1. Linear Traversal on LinkedList
+        while(temp != null) {
+// 2. Concatenating current node's data to the existing 'str'.
+//    And reassigning to 'str' again.
+            str += temp.data;          
+            temp = temp.next;
+        }
+        
+        int n = str.length();
+// 3. Checking for palindrome or not.
+        for(int i = 0; i < (int) n/2; i++) {
+            if(str.charAt(i) != str.charAt(n-i-1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
