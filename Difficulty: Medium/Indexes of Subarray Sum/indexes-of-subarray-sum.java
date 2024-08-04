c4fb48c1c9d0699be846a4e8c53101b1ee7317b6
Tanesha Gahtori <104068017+taneshagahtori@users.noreply.github.com>
2024-08-04 22:13:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Indexes of Subarray Sum/indexes-of-subarray-sum.java
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class IntArray {
+    public static int[] input(BufferedReader br, int n) throws IOException {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a) {
+        StringBuilder sb = new StringBuilder();
+        for (int e : a) sb.append(e + " ");
+        System.out.println(sb);
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            String str[] = br.readLine().trim().split(" ");
+
+            int n = Integer.parseInt(str[0]);
+            int s = Integer.parseInt(str[1]);
+
+            int[] arr = IntArray.input(br, n);
+
+            Solution obj = new Solution();
+            ArrayList<Integer> res = obj.subarraySum(arr, n, s);
+
+            IntArray.print(res);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public static ArrayList<Integer> subarraySum(int[] arr, int n, int s) {
+        // code here
+        ArrayList<Integer> al = new ArrayList<>();
+        int sum = 0;
+        int i = 0;
+
+        for(int j=0;j<n;j++) {
+            sum += arr[j];
+
+            while (sum > s && i <= j) {
+                sum -= arr[i];
+                i++;
+            }
+
+            if (sum == s && i<=j) {
+                al.add(i + 1);  // 1-based index for the start
+                al.add(j + 1);  // 1-based index for the end
+                return al;
+            }
+
+            
+        }
+
+        al.add(-1);  // No subarray found
+        return al;
+    
+    }
+    
+}
