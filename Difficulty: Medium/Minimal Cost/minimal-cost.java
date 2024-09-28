5ca8b92b6658ac4026ef59c1769dbd60fa3a622b
Tanesha Gahtori <104068017+taneshagahtori@users.noreply.github.com>
2024-09-28 16:33:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimal Cost/minimal-cost.java
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+// User function Template for Java
+
+class Solution {
+    public int minimizeCost(int k, int arr[]) {
+        int n = arr.length;
+        int[] dp = new int[n];
+        
+        // Initializing the dp array with maximum values
+        for (int i = 1; i < n; i++) {
+            dp[i] = Integer.MAX_VALUE;
+        }
+        
+        // Base case: no cost to stay on the first stone
+        dp[0] = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j <= k; j++) {
+                if (i + j < n) {
+                    dp[i + j] = Math.min(dp[i + j], dp[i] + Math.abs(arr[i] - arr[i + j]));
+                }
+            }
+        }
+
+        return dp[n - 1];
+    }
+}
+
+
+//{ Driver Code Starts.
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            int k = Integer.parseInt(br.readLine());
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+            Solution obj = new Solution();
+            int res = obj.minimizeCost(k, arr);
+
+            System.out.println(res);
+        }
+    }
+}
+// } Driver Code Ends
