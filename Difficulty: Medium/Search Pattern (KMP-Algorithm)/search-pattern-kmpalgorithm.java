77dde6e7f09e52715ec27658f33c782909b831a3
Tanesha Gahtori <104068017+taneshagahtori@users.noreply.github.com>
2024-12-02 22:25:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Search Pattern (KMP-Algorithm)/search-pattern-kmpalgorithm.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            String s, patt;
+            s = sc.next();
+            patt = sc.next();
+
+            Solution ob = new Solution();
+
+            ArrayList<Integer> res = ob.search(patt, s);
+            if (res.size() == 0)
+                System.out.print("[]");
+            else {
+                for (int i = 0; i < res.size(); i++) System.out.print(res.get(i) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+
+    ArrayList<Integer> search(String pat, String txt) {
+        // your code here
+        
+       
+         ArrayList<Integer> res= new ArrayList<Integer>();
+         int l=pat.length();
+         
+         for(int i=0;i<=txt.length()-l;i++){
+             if(txt.substring(i,l+i).equals(pat)){
+                 res.add(i);
+             }
+             
+         }
+         
+         
+         return res;
+        
+    }
+}
